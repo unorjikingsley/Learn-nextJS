717ce7f25f59cb4e55642b622fc334c5173806b8
unorji kingsley <unorjikingsley@gmail.com>
2023-12-21 15:46:04 +0100

    Add google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* <body>{children}</body> */}
+      {/* you're also adding the Tailwind antialiased class which smooths out the
+      // font. It's not necessary to use this class, but it adds a nice touch. */}
+
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import styles from '@/app/ui/home.module.css';
+import { lusitana } from '@/app/ui/fonts';
+// import { lusitana } from './ui/fonts';
 
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
@@ -8,11 +10,12 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
       
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div className={`${lusitana.className} antialiased`}>
+          {/* className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20"> */}
           {/* <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" /> - same is achieved using tailwind */}
           <div className={styles.shape}></div>
 
