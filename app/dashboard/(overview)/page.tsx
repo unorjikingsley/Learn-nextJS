5e20da7b54408ec53e2ae09da989eef5777d4bb1
unorji kingsley <unorjikingsley@gmail.com>
2023-12-22 15:37:58 +0100

    Streaming

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,22 @@
-import { Card } from '@/app/ui/dashboard/cards';
+// import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
 import {
-  fetchRevenue,
-  fetchLatestInvoices,
+  // fetchRevenue,
+  // fetchLatestInvoices,
   fetchCardData,
 } from '@/app/lib/data';
+
+  import { Suspense } from 'react';
+  import {
+    LatestInvoicesSkeleton,
+    RevenueChartSkeleton,
+    CardsSkeleton,
+  } from '@/app/ui/skeletons';
+
+import CardWrapper from '@/app/ui/dashboard/cards';
 
 export default async function Page() {
   const {
@@ -17,8 +26,8 @@ export default async function Page() {
     totalPendingInvoices,
   } = await fetchCardData();
 
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const revenue = await fetchRevenue();
+  // const latestInvoices = await fetchLatestInvoices();
 
   return (
     <main>
@@ -26,18 +35,32 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
         <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        />
+        /> */}
+
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
+
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        {/* <RevenueChart revenue={revenue}  /> */}
+
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
